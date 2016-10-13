61d68bf5b23d4461c25f91d7f8210974116a93ea
apsingh7 <asapsingh7@gmail.com>
2016-10-13 13:36:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one requested and will send requested file');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two requested and will send requested file');
+});
+app.get('/article-three', function (req, res) {
+  res.send('Article three requested and will send requested file');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
