9ee59abd11d7a9e721c0afa93410a8fde80f9f18
apsingh7 <asapsingh7@gmail.com>
2016-10-14 01:56:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,22 @@
 //var button = document.getElementById('counter');
 var counter=0;
 window.onload = function(){
-document.getElementById('counter').onclick = function() {
-	counter =counter+1;
-	var span=document.getElementById('count');
+document.getElementById('counter').onclick = function(){
+    
+ var request = new XMLHttpRequest();
+request.onreadystatechange = function()
+{
+	if(request.readyState===XMLHttpRequest.DONE){
+	if(request.status===200)
+	{
+		var counter=request.responseText;
+			var span=document.getElementById('count');
 span.innerHTML=counter.toString();
-}
-}
+	}		
+	}
+	//make the request
+	request.open('GET' ,'http://apsingh7.imad.hasura-app.io/counter',true );
+	request.send(null);
+};
+};
+};
