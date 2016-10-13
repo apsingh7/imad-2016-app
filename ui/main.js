75ddb535c6048faa7a927cdab8f982683fb204be
apsingh7 <asapsingh7@gmail.com>
2016-10-14 03:11:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,18 +11,34 @@ span.innerHTML=counter.toString();
 
 
 
-var nameInput=document.getElementById('name');
-//var name = nameInput.value;
-window.onload = function(){
-document.getElementById('submit').onclick=function(){
-	var names=['name1','name2','name3'];
+
+
+var submit=document.getElementById('submit');
+submit.onclick=function(){
+
+	var request=new XMLHttpRequest();
+	request.onreadystatechange=function()
+{
+if(request.readyState===XMLHttpRequest.DONE)
+{
+if(request.status===200){
+
+	var names=request.responseText;
+	names=JSON.parse(names);
 	var list='';
-	for(var i=0;i<names.length;i++)
+	for(var i=0;i<name.length;i++)
 	{
 		list +='<li>'+names[i]+'</li>';
 		
 	}
+
 	var ul=document.getElementById('namelist');
 	ul.innerHTML=list;
+}
+}
 };
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://apsingh7.imad.hasura-app.io/submit-name='+name,true);
+request.send(null);
 };
