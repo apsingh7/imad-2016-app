92ab1a799f2b73772e7bdd65cf48f0d032f0a980
apsingh7 <asapsingh7@gmail.com>
2016-10-14 02:37:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var counter=0;
 window.onload = function(){
 document.getElementById('counter').onclick=function()
 {
-	conter =counter+1;
+	counter =counter+1;
 	var span=document.getElementById('count');
 span.innerHTML=counter.toString();
 };
