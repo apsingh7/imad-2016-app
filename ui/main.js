b7abc3af42c2471c1dbb67b62425208634eac1e6
apsingh7 <asapsingh7@gmail.com>
2016-10-14 02:27:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ span.innerHTML=counter.toString();
 };
 
 var nameInput=document.getElementById('name');
-var name = nameInput.value;
+//var name = nameInput.value;
 var submit=document.getElementById('submit');
 submit.onclick=function(){
 	var names=['name1','name2','name3'];
