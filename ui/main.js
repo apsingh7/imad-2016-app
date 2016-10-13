a48921d1dbed9e3988c9a01c42c8b310ba62a8ac
apsingh7 <asapsingh7@gmail.com>
2016-10-14 02:34:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,24 @@
 var counter=0;
-var button = document.getElementById('counter');
-
-button.onclick=function()
+window.onload = function(){
+document.getElementById('counter').onclick=function()
 {
 	conter =counter+1;
 	var span=document.getElementById('count');
 span.innerHTML=counter.toString();
 };
+};
 
+var nameInput=document.getElementById('name');
+//var name = nameInput.value;
+var submit=document.getElementById('submit');
+submit.onclick=function(){
+	var names=['name1','name2','name3'];
+	var list='';
+	for(var i=0;i<names.length;i++)
+	{
+		list ='<li>'+names[i]+'</li>';
+		
+	}
+	var ul=document.getElementById('namelist');
+	ul.innerHTML=list;
+};
