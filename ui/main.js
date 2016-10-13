13d281327595c338cb1710a3bccf8bbba36685aa
apsingh7 <asapsingh7@gmail.com>
2016-10-13 23:32:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+console.log('Loaded!');
+
+var element=document.getElementById('main-text');
+element.innerHTML="New Value";
+
+//Move the Image.
+
+var img=document.getElementById('modi');
+var marginLeft=0; 
+function moveRight()
+{
+	marginLeft=marginLeft+10;
+	img.style.marginLeft=marginLeft + 'px';
+}
+img.onclick=function(){
+	var interval=setInterval(moveRight,100);
+	
+};
