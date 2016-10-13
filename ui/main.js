c9423040de6409921ba67101d0233dd075cafb21
apsingh7 <asapsingh7@gmail.com>
2016-10-14 02:35:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,19 +7,4 @@ document.getElementById('counter').onclick=function()
 span.innerHTML=counter.toString();
 };
 
-
-var nameInput=document.getElementById('name');
-//var name = nameInput.value;
-var submit=document.getElementById('submit');
-submit.onclick=function(){
-	var names=['name1','name2','name3'];
-	var list='';
-	for(var i=0;i<names.length;i++)
-	{
-		list ='<li>'+names[i]+'</li>';
-		
-	}
-	var ul=document.getElementById('namelist');
-	ul.innerHTML=list;
-};
 };
