e4101c2a3affe3803b487a71e1af3f1df3bf72b5
AJAY PRATAP SINGH <asapsingh7@gmail.com>
2016-11-28 15:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -230,7 +230,8 @@ body {
               </div>
           </div>
           <script type="text/javascript" src="/ui/article.js"></script>
-
+<script type="text/javascript" src="/ui/main.js">
+        </script>
 </body>
 </html>
 
